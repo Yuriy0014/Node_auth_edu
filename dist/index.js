6d3d6cfb72bdaf511bb62c6ad2743e6c7ee3b6b2
YurL0014 <ylogachev-sfedu@mail.ru>
2023-06-04 08:47:48 +0300

    Switched mongo URL to separate file

--- dist/index.js
+++ dist/index.js
@@ -8,17 +8,23 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const auth_router_1 = require("./routes/auth-router");
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const app = express();
 const port = process.env.PORT || 3000;
+const mongoUri = process.env.MONGO_URL;
 app.use(express.json());
 app.use('/auth', auth_router_1.authRouter);
 const start = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        yield mongoose.connect('mongodb+srv://ylogachev2019:qwerty123@cluster0.jb9egoo.mongodb.net/?retryWrites=true&w=majority');
+        yield mongoose.connect(mongoUri);
         app.listen(port, () => {
             console.log(`Example app listening on port ${port}`);
         });
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,19 @@ import {authRouter} from "./routes/auth-router";
 const express = require('express')
 const mongoose = require('mongoose')
 
+import dotenv from 'dotenv'
+dotenv.config()
+
 const app = express()
 const port = process.env.PORT || 3000
-
+const mongoUri = process.env.MONGO_URL
 app.use(express.json())
 
 app.use('/auth',authRouter)
 
 const start = async () => {
     try {
-        await mongoose.connect('mongodb+srv://ylogachev2019:qwerty123@cluster0.jb9egoo.mongodb.net/?retryWrites=true&w=majority');
+        await mongoose.connect(mongoUri);
         app.listen(port, () => {
             console.log(`Example app listening on port ${port}`)
         })
